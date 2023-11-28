c6e23908104235c1d0ab3e8de7385ac02c1ec21c
Anoxta99 <wongklowor99@gmail.com>
2023-11-28 18:24:47 +0700

    aye

--- project.py
+++ project.py
@@ -18,7 +18,7 @@ st.subheader('Prediksi gaji per tahun untuk karyawan di Amerika Serikat')
 
 with st.form("Penngalaman Kerja"):
 
-    number = st.number_input("Pengalaman Kerja (Tahun)", format="%0.1f", min_value=0.0, placeholder="Masukkan angka...")
+    number = st.number_input("Pengalaman Kerja (Tahun)", format="%0.1f", min_value=0.0, step=0.1, placeholder="Masukkan angka...")
 
     submitted = st.form_submit_button("Submit")
 
